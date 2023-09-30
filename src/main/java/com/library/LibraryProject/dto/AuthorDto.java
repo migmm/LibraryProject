b8a58ad843e313fce7d6f7d08bb71e77999ac897
Miguel M <miguelm@hotmail.com.ar>
2023-09-30 17:13:59 -0300

    Created dtos

---
+++ src/main/java/com/library/LibraryProject/dto/AuthorDto.java
@@ -0,0 +1,21 @@
+package com.library.LibraryProject.dto;
+
+import com.library.LibraryProject.model.Book;
+import lombok.*;
+
+import java.util.Set;
+
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AuthorDto {
+
+    private String name;
+    private String surname;
+    private Set<Book> books;
+}
+
---
+++ src/main/java/com/library/LibraryProject/dto/BookDto.java
@@ -0,0 +1,23 @@
+package com.library.LibraryProject.dto;
+
+import com.library.LibraryProject.model.Author;
+import lombok.*;
+
+import java.util.Date;
+import java.util.Set;
+
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class BookDto {
+
+    private String title;
+    private Date publishDate;
+    private String genre;
+    private Set<Author> authors;
+
+}
